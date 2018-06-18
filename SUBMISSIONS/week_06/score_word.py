b8ba3edca2b8c7bebc5e99306643f158fb7eb32d
ramirocadavid <ramirocadavid@gmail.com>
2018-06-18 13:05:21 -0500

    Week 6 homework complete

--- SUBMISSIONS/week_06/score_word.py
+++ SUBMISSIONS/week_06/score_word.py
@@ -1,9 +1,9 @@
 def score_word(word, score_table):
-    """ Receives a word and calculate its total score
+    """ Receives a word and calculates its total score
         by adding up the score of each letter in word.
-        Score is found in the score table entered.
+        Score is found in the score_table entered.
     """
     score = 0
-    for letter in word:
+    for letter in word.lower():
         score += score_table[letter]
     return score
